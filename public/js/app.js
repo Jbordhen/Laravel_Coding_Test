10021249b67bc97620493e77d326d12768381b89
Jbordhen <24916865+Jbordhen@users.noreply.github.com>
2021-07-27 10:32:11 +0600

    testing

--- public/js/app.js
+++ public/js/app.js
@@ -63300,8 +63300,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/rifat/Programming/mediusware/interview/interview-question-sr/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/rifat/Programming/mediusware/interview/interview-question-sr/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Joy Bordhen\Desktop\test\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Joy Bordhen\Desktop\test\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
